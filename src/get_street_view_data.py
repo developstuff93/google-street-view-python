c04b8627f13a86c5a3d5789c729ff5815c224536
developstuff93 <developstuff93@gmail.com>
2020-03-13 08:01:07 -0400

    add exception handling to street view metadata request

--- src/get_street_view_data.py
+++ src/get_street_view_data.py
@@ -5,11 +5,15 @@ from constants import STREET_IAMGE_KEY, STREET_METADATA_URL, STREET_IMAGE_URL, I
 
 API_KEY = STREET_IAMGE_KEY
 
+
 def extract_street_view_metadata(address_or_latlng):
     location = address_or_latlng
     endpoint = f"{STREET_METADATA_URL}?size={IMAGE_SIZE}&fov={FOV}&location={location}&key={API_KEY}"
-    res = requests.get(endpoint)
-    return res.json()
+    try:
+        res = requests.get(endpoint)
+        return res.json()
+    except:
+        return None
 
 
 def extract_street_view_image_url(address_or_latlng):
